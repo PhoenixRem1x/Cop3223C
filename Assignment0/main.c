adf75c15751d53f6b09b259c5c66a29306080beb
Parker Wales <parker9145@gmail.com>
2026-08-25 19:28:56 -0400

    Assignment 0

---
+++ Assignment0/main.c
@@ -0,0 +1,14 @@
+/*
+Parker Wales
+pa945111 @ucf.edu
+Assignment 0
+Dr. Rahaman
+COP 3233C - Intro to C Programming
+*/
+
+#include <stdio.h>
+int main(void) {
+    printf("Welcome to COP3223.\n");
+    printf("NID: %s\n","pa945111");
+    return 0;
+}
---
+++ Program0/main.c
@@ -0,0 +1,13 @@
+/*
+Author: Parker Wales
+Course: COP3223C
+Assignment: Program0
+Date: August 25, 2026
+*/
+
+#include <stdio.h>
+int main(void) {
+    printf("Welcome to COP3223.\n");
+    printf("NID: %s\n","pa945111");
+    return 0;
+}
