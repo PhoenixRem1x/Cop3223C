28bfe09b60f6bccc7ee1c55a199a7d20765e8005
Parker Wales <parker9145@gmail.com>
2026-09-11 13:30:16 -0400

    Fixed the folder with the proper assignment's name

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -12,9 +12,9 @@ int main(void){
 
 	float x1,y1,x2,y2,distance,area; //All the variables
 
-	printf("Please enter the first point (x1,y1): ");
+	printf("Please enter the first point x1 y1: ");
 	scanf("%f %f",&x1,&y1); // Store floats into (x1,y1)
-	printf("Please enter the second point (x2,y2): ");
+	printf("Please enter the second point x2 y2): ");
 	scanf("%f %f",&x2,&y2); // Store floats into (x2,y2)
 	
 	distance = sqrt(pow((x2-x1),2) + pow((y2-y1),2) ); //Calculates distance
